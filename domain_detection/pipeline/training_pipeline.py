e365e0a3c7520bd1a7915e97bd3502eada4e383b
DeewanThapa <karan.thapa37@gmail.com>
2023-08-25 12:10:30 +0530

    project updated

--- domain_detection/pipeline/training_pipeline.py
+++ domain_detection/pipeline/training_pipeline.py
@@ -91,16 +91,16 @@ class TrainPipeline:
 
     def sync_artifact_dir_to_s3(self):
         try:
-            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
             self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.artifact_dir,
-                                           aws_buket_url=aws_buket_url)
+                                           aws_buket_url=aws_bucket_url)
         except Exception as e:
             raise CustomException(e, sys)
 
     def sync_saved_model_dir_to_s3(self):
         try:
-            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
-            self.s3_sync.sync_folder_to_s3(folder=SAVED_MODEL_DIR, aws_buket_url=aws_buket_url)
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
+            self.s3_sync.sync_folder_to_s3(folder=SAVED_MODEL_DIR, aws_buket_url=aws_bucket_url)
         except Exception as e:
             raise CustomException(e, sys)
 
@@ -121,7 +121,7 @@ class TrainPipeline:
             TrainPipeline.is_pipeline_running = False
             self.sync_artifact_dir_to_s3()
             self.sync_saved_model_dir_to_s3()
-        except  Exception as e:
+        except Exception as e:
             self.sync_artifact_dir_to_s3()
             TrainPipeline.is_pipeline_running = False
             raise CustomException(e, sys)
